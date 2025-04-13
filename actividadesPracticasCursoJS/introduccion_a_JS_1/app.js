0f8211b1884da218a49e6463a5e120aaa89e475e
EdderFlores <floresmoy11@gmail.com>
2025-04-13 02:40:08 -0600

    Created a new folder for new excercises and organized certain details for my general documents (notes, live activities, self-learning)

---
+++ actividadesPracticasCursoJS/introduccion_a_JS_1/app.js
@@ -0,0 +1,16 @@
+//Crea variables con los siguientes tipos de datos (Number, string y boolean)
+//Muestra los valores de las variables en consola
+
+let numero;
+let texto = "";
+let booleano = true;
+
+numero = parseInt(numero);
+
+numero = 25;
+texto = "Licenciado Valeriano";
+booleano = false;
+
+console.log(`Numero: ${numero} y es ${typeof numero}`);
+console.log(`Texto: ${texto} y es ${typeof texto}`);
+console.log(`Booleano: ${booleano} y es ${typeof booleano}`);
