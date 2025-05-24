c2ae9772ec62fef5a66fca2d6bb9d0c13eb1c8ac
EdderFlores <floresmoy11@gmail.com>
2025-05-24 03:11:02 -0600

    Created a task manager app that does basic summing tasks in a list

---
+++ generalProjectsFromCoderhouse/Entregrable1_Flores/app.js
@@ -0,0 +1,58 @@
+// Recepcion de datos
+let tasks = [];
+
+// funcion para pedir datos
+const addTask = () => {
+  const newTask = prompt("What task do you want to add");
+  if (newTask) {
+    tasks.push(newTask);
+    alert("Added " + newTask);
+  }
+};
+
+// funcion para ver tareas actuales
+
+function viewTasks() {
+  if (tasks.length === 0) {
+    console.log("No tasks have been added yet");
+  } else {
+    console.log("Current tasks:\n" + tasks.join("\n"));
+  }
+}
+
+// start menu para agregar tareas
+
+const showMenu = () =>
+  prompt(
+    "Task Menu:\n" +
+      "1. Add Task\n" +
+      "2. View Tasks\n" +
+      "3. Exit\n" +
+      "Enter your choice from 1-4:"
+  );
+
+// Procesamiento principal de programa
+
+let userChoise;
+do {
+  userChoise = showMenu();
+
+  switch (userChoise) {
+    case "1":
+      addTask();
+      break;
+    case "2":
+      viewTasks();
+      break;
+    case "3":
+      alert("Exiting the Task Manager Program... Bye!");
+      break;
+    case "4":
+      alert("Exiting the Task Manager Program... Bye!");
+      break;
+    default:
+      console.log("Please enter a valid choise from 1-3");
+  }
+} while (userChoise !== "3");
+
+viewTasks();
