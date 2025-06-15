2121e61e123488955dd2a36de457be7b25e88e25
EdderFlores <floresmoy11@gmail.com>
2025-06-14 22:21:36 -0600

    updated old app version to not do an infinite loop

--- generalProjectsFromCoderhouse/Entregrable1_Flores/app.js
+++ generalProjectsFromCoderhouse/Entregrable1_Flores/app.js
@@ -1,17 +1,16 @@
-// Recepcion de datos
+// Recepción de datos
 let tasks = [];
 
-// funcion para pedir datos
+// Función para pedir datos
 const addTask = () => {
-  const newTask = prompt("What task do you want to add");
+  const newTask = prompt("What task do you want to add?");
   if (newTask) {
     tasks.push(newTask);
-    alert("Added " + newTask);
+    alert("Added: " + newTask);
   }
 };
 
-// funcion para ver tareas actuales
-
+// Función para ver tareas actuales
 function viewTasks() {
   if (tasks.length === 0) {
     console.log("No tasks have been added yet");
@@ -20,40 +19,38 @@ function viewTasks() {
   }
 }
 
-// start menu para agregar tareas
-
+// Menú principal
 const showMenu = () =>
   prompt(
-    "Make sure to open the console log to see the app in action, to reopen the app just refresh the page\n" +
+    "Make sure to open the console log to see the app in action\n" +
       "Task Menu:\n" +
       "1. Add Task\n" +
       "2. View Tasks\n" +
       "3. Exit\n" +
-      "Enter your choice from 1-4:"
+      "Enter your choice (1-3):"
   );
 
-// Procesamiento principal de programa
+// Procesamiento principal del programa
+function runTaskManager() {
+  let userChoice;
+  do {
+    userChoice = showMenu();
 
-let userChoise;
-do {
-  userChoise = showMenu();
+    switch (userChoice) {
+      case "1":
+        addTask();
+        break;
+      case "2":
+        viewTasks();
+        break;
+      case "3":
+        alert("Exiting the Task Manager Program... Bye!");
+        break;
+      default:
+        alert("Please enter a valid choice from 1-3");
+    }
+  } while (userChoice !== "3");
+}
 
-  switch (userChoise) {
-    case "1":
-      addTask();
-      break;
-    case "2":
-      viewTasks();
-      break;
-    case "3":
-      alert("Exiting the Task Manager Program... Bye!");
-      break;
-    case "4":
-      alert("Exiting the Task Manager Program... Bye!");
-      break;
-    default:
-      console.log("Please enter a valid choise from 1-3");
-  }
-} while (userChoise !== "3");
-
-viewTasks();
+// Iniciar la aplicación
+runTaskManager();
